d75b0dc8fea8f071adeafb0817f2124b2019fd27
arjun <arjunkan2003@gmail.com>
2024-05-14 12:48:52 -0500

    Moved model.py code to app.py and tested in Flask application. Created standard form for frontend

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
-from flask import Flask, request, jsonify
-from model import score
+from flask import Flask, request, jsonify, render_template
+from transformers import BertTokenizer, BertForSequenceClassification
+import torch
 
 app = Flask(__name__)
 
-@app.route('/score', methods=['POST'])
-def score():
-    data = request.json
-    text = data['text']
-    result = score(text)
-    return jsonify({'quality_score': result})
+bertBase = "bert-base-uncased" # bert-large is too expensive
+tokenizer = BertTokenizer.from_pretrained(bertBase)
+model = BertForSequenceClassification.from_pretrained(bertBase)
+
+def score_arg(text):
+    encoded_inputs = tokenizer(text, return_tensors="pt", truncation=True)
+    outputs = model(**encoded_inputs)
+
+    probabilities = torch.softmax(outputs.logits, dim=-1).detach().numpy()
+
+    return probabilities[:, 1].item()
 
 @app.route("/")
 def home():
-    return "Welcome to the LawyerAI!"
+    return render_template('index.html')
+
+@app.route('/evaluate', methods=['POST'])
+def score():
+    data = request.form['text']
+    score = score_arg(data)
+    return jsonify({'Argument': data, 'Quality Score': score})
 
 if __name__ == "__main__":
     app.run(debug=True)
