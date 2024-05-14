55010774a9030d48c113545628fde70297aec0a2
arjun <arjunkan2003@gmail.com>
2024-05-13 20:02:10 -0500

    Model produces scores on test data

--- model.py
+++ model.py
@@ -1,13 +1,15 @@
 from transformers import BertTokenizer, BertForSequenceClassification
 import torch
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 bertBase = "bert-base-uncased" # bert-large is too expensive
 tokenizer = BertTokenizer.from_pretrained(bertBase)
-model = BertForSequenceClassification.from_pretrained(bertBase, num_labels=1)
+model = BertForSequenceClassification.from_pretrained(bertBase, num_labels=2)
 
 def score(text):
 
-    inputs = tokenizer(text, do_lower_case=True, return_tensors="pt", max_length=512, truncation=True, padding="max_length")
+    inputs = tokenizer(text, return_tensors="pt", max_length=512, truncation=True, padding="max_length")
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
@@ -22,3 +24,13 @@ def score(text):
 
     # Positive class probability
     return probabilities[0][1].item()
+
+data = pd.read_csv("./data/arg_quality_rank_30k.csv")
+argument = data['argument']
+quality = data['WA']
+
+train_texts, test_texts, train_labels, test_labels = train_test_split(argument, quality, test_size=0.2, random_state=42)
+
+for text in test_texts.sample(5):  # testing with 5 random samples
+    print('Text:', text)
+    print('Score:', score(text), '\n')
