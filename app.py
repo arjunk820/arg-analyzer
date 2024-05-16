bb339e1092bf009b2f6523219ff22c63b3d6f7d3
arjun <arjunkan2003@gmail.com>
2024-05-16 15:21:51 -0500

    Added function contracts and important comments in app.py

--- app.py
+++ app.py
@@ -8,11 +8,18 @@ bertBase = "bert-base-uncased"
 tokenizer = BertTokenizer.from_pretrained(bertBase)
 model = BertForSequenceClassification.from_pretrained(bertBase)
 
+# evaluate()
+# Inputs: string
+# Returns: float
+# Expects: a string with text within 16-512 characters
+# Notes: The function uses the tokenizer and model instantiated above
+# Purpose: The function processes the text and gives a quality score
+
 def evaluate(text):
 
     text = ' '.join(text.split())
 
-    if len(text) < 10:
+    if len(text) < 16:
         return 0.0
 
     encoded_inputs = tokenizer(text, return_tensors="pt", truncation=True, max_length=512, padding=True)
@@ -24,15 +31,31 @@ def evaluate(text):
 
     return probabilities[:, 1].item()
 
+
+# home()
+# Inputs: N/A
+# Returns: render_template(index.html)
+# Expects: index.html in proj directory
+# Notes: N/A
+# Purpose: This function presents index.html on running the application
+
 @app.route("/")
 def home():
     return render_template('index.html')
+
+# score()
+# Inputs: N/A
+# Returns: data (text) and evaluate(data)
+# Expects: input in the form in the app
+# Notes: N/A
+# Purpose: This function calls evaluate() and returns the input text and score in json format
 
 @app.route('/evaluate', methods=['POST'])
 def score():
     data = request.form['text']
     score = evaluate(data)
     
+    # Outputting the result in % form for readability
     score = round(100 * score, 2)
 
     return jsonify({'Argument': data, 'Quality Score': score})
